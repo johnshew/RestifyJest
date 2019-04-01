39763dee386314cb4311a39c4b05575dc8a14830
John Shewchuk <johnshew@users.noreply.github.com>
2019-04-01 08:28:27 -1000

    Testing promises with await

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -1,12 +1,33 @@
 import { Server } from '../app';
 
-test('basic',  (done) => {
-    let server = new Server(()=>{
+test('basic', async (done) => {
+    let server = new Server(() => {
         console.log('Listening');
-        server.close(()=>{
+        server.close(() => {
             console.log('Closed');
             done();
         });
     });
 
+});
+
+
+test('test repeat', async (done) => {
+    let server = new Server(() => {
+        console.log('Listening');
+        server.close(() => {
+            console.log('Closed');
+            done();
+        });
+    });
+
+});
+
+test('test repeat with await', async (done) => {
+    let server = new Server();
+    await server.ready;
+    console.log('Listening');
+    await server.close();
+    console.log('Closed');
+    done();
 });
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,28 @@
 
 import * as restify from 'restify';
+import { readSync } from 'fs';
 
 export class Server {
     server: restify.Server;
-    // ready : Promise<void>;
+    ready: Promise<void>;
+    closed : Promise<void>;
 
     constructor(callback?: () => any) {
         this.server = restify.createServer();
-        // this.ready = new Promise<void>((resolve, reject) => {));
-        this.server.listen(8080,callback);
+        this.ready = new Promise<void>((resolve, reject) => {
+            this.server.listen(8080, () =>{
+                callback && callback();
+                return resolve()
+            });
+        });
     }
-    
-    close(callback: () => any) {
-        this.server.close(callback);
+
+    close(callback?: () => any) : Promise<void> {
+        return new Promise((resolve,reject)=>{
+            this.server.close(()=>{
+                callback && callback();
+                return resolve();
+            });
+        });
     }
-} 
+}
